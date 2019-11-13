8af7e27a5bb46c4d51547fa389448efb0b44b304
Alex Hoffman <alxhoff@gmail.com>
2019-11-13 16:54:29 +0100

    Find stdperiph locates components

--- cmake/FindSTDPERIPH.cmake
+++ cmake/FindSTDPERIPH.cmake
@@ -1,62 +1,51 @@
-IF(NOT STM32_CHIP_TYPE)
-    MESSAGE(FATAL_ERROR "Select your stm32 chip using STM32_CHIP_TYPE variable. (HD, HD_VL, MD, MD_VL, LD, LD_VL, XD, CL)")
+IF(STM32_FAMILY STREQUAL "F4")
+    SET(STD_COMPONENTS adc can cec crc cryp dac dbgmcu dcmi dfsdm dma2d dma dsi
+        exti flash flash_ramfunc fmc fmpi2c fsmc gpio hash i2c iwdg lptim ltdc pwr qspi rcc
+        rng rtc sai sdio spdifrx spi syscfg tim usart wwdg)
+
+    SET(STD_REQUIRED_COMPONENTS dma dma2d fmc i2c ltdc gpio rcc spi usart adc tim exti syscfg)
+
+    SET(STD_EX_COMPONENTS cryp_aes cryp_des cryp_tdes hash_md5 hash_shal)
+
+    SET(STD_PREFIX stm32f4xx_)
 ENDIF()
 
-IF(STM32_CHIP_TYPE STREQUAL "HD")
-    SET(StdPeriphLib_LIB_NAME stdperiph_hd)
-ELSEIF(STM32_CHIP_TYPE STREQUAL "HD_VL")
-    SET(StdPeriphLib_LIB_NAME stdperiph_hd_vl)
-ELSEIF(STM32_CHIP_TYPE STREQUAL "MD")
-    SET(StdPeriphLib_LIB_NAME stdperiph_md)
-ELSEIF(STM32_CHIP_TYPE STREQUAL "MD_VL")
-    SET(StdPeriphLib_LIB_NAME stdperiph_md_vl)
-ELSEIF(STM32_CHIP_TYPE STREQUAL "LD")
-    SET(StdPeriphLib_LIB_NAME stdperiph_ld)
-ELSEIF(STM32_CHIP_TYPE STREQUAL "LD_VL")
-    SET(StdPeriphLib_LIB_NAME stdperiph_ld_vl)
-ELSEIF(STM32_CHIP_TYPE STREQUAL "XD")
-    SET(StdPeriphLib_LIB_NAME stdperiph_xd)
-ELSEIF(STM32_CHIP_TYPE STREQUAL "CL")
-    SET(StdPeriphLib_LIB_NAME stdperiph_cl)
-ELSE()
-    MESSAGE(FATAL_ERROR "Invalid stm32 chip type.")
+SET(STD_HEADERS
+    misc.h
+    )
+
+SET(STD_SOURCES
+    misc.c
+    )
+
+IF(NOT STM32STD_FIND_COMPONENTS)
+    SET(STM32STD_FIND_COMPONENTS ${STD_COMPONENTS})
+    MESSAGE(STATUS "No STM32STD components selected, using all: ${$STM32STD_FIND_COMPONENTS}")
 ENDIF()
 
-FIND_PATH(StdPeriphLib_INCLUDE_DIR
-    stm32f10x_adc.h
-    stm32f10x_bkp.h
-    stm32f10x_can.h
-    stm32f10x_cec.h
-    stm32f10x_crc.h
-    stm32f10x_dac.h
-    stm32f10x_dbgmcu.h
-    stm32f10x_dma.h
-    stm32f10x_exti.h
-    stm32f10x_flash.h
-    stm32f10x_fsmc.h
-    stm32f10x_gpio.h
-    stm32f10x_i2c.h
-    stm32f10x_iwdg.h
-    stm32f10x_pwr.h
-    stm32f10x_rcc.h
-    stm32f10x_rtc.h
-    stm32f10x_sdio.h
-    stm32f10x_spi.h
-    stm32f10x_tim.h
-    stm32f10x_usart.h
-    stm32f10x_wwdg.h
-    stm32f10x_misc.h
-    PATH_SUFFIXES include stm32
-    )
+FOREACH(cmp ${STD_REQUIRED_COMPONENTS})
+    LIST(FIND STM32STD_FIND_COMPONENTS ${cmp} STM32STD_FOUND_INDEX)
+    IF(${STM32STD_FOUND_INDEX} LESS 0)
+        LIST(APPEND STM32STD_FIND_COMPONENTS ${cmp})
+    ENDIF()
+ENDFOREACH()
 
-FIND_LIBRARY(StdPeriphLib_LIBRARIES
-    NAMES ${StdPeriphLib_LIB_NAME}
-    PATH_SUFFIXES lib
-    )
+FOREACH(cmp ${STM32STD_FIND_COMPONENTS})
+    LIST(FIND STD_COMPONENTS ${cmp} STM32STD_FOUND_INDEX)
+    IF($STM32STD_FOUND_INDEX LESS 0)
+        MESSAGE(FATAL_ERROR "Unknown STM32STD Peripheral component: ${cmp}. Available components: ${STD_COMPONENTS}")
+    ELSE()
+        LIST(APPEND STD_HEADERS ${STD_PREFIX}${cmp}.h)
+        LIST(APPEND STD_SOURCES ${STD_PREFIX}${cmp}.c)
+    ENDIF()
+    LIST(FIND HAL_EX_COMPONENTS ${cmp} STM32STD_FOUND_INDEX)
+    if(NOT (${STM32STD_FOUND_INDEX} LESS 0))
+        LIST(APPEND STD_SOURCES ${STD_PREFIX}${cmp}.c)
+    ENDIF()
+ENDFOREACH()
 
-SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DUSE_STDPERIPH_DRIVER")
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DUSE_STDPERIPH_DRIVER")
+LIST(REMOVE_DUPLICATES STD_HEADERS)
+LIST(REMOVE_DUPLICATES STD_SOURCES)
 
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(StdPeriphLib DEFAULT_MSG StdPeriphLib_LIBRARIES StdPeriphLib_INCLUDE_DIR)
-
+MESSAGE("HEADERS: ${STD_HEADERS}")
+MESSAGE("SOURCES: ${STD_SOURCES}")
